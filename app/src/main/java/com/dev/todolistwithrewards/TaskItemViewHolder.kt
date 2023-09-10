ac210caa2f2805e497d80b8d546c7a68ace4abca
realp <realprof.mx@gmail.com>
2023-09-10 17:50:59 +0200

    TDLWR-6 create a score page

--- app/src/main/java/com/dev/todolistwithrewards/TaskItemViewHolder.kt
+++ app/src/main/java/com/dev/todolistwithrewards/TaskItemViewHolder.kt
@@ -1,8 +1,13 @@
 package com.dev.todolistwithrewards
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.view.Gravity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.dev.todolistwithrewards.databinding.TaskItemCellBinding
 import java.time.format.DateTimeFormatter
@@ -13,15 +18,23 @@ class TaskItemViewHolder(
     private val clickListener: TaskItemClickListener
 ) : RecyclerView.ViewHolder(binding.root) {
     val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+    val color = ContextCompat.getColor(context, R.color.custom_red)
     fun bindTaskItem(taskItem: TaskItem) {
         binding.name.text = taskItem.name
         binding.score.text = taskItem.score.toString()
-        binding.score.setTextColor(Color.parseColor("#FF07A61D"))
+        binding.score.setTextColor(Color.parseColor("#FF0786CA"))
+        binding.score.setBackgroundResource(R.drawable.yellow_circle)
+
+        resizeBackgroundImage(binding.score.background)
+
 
         if (taskItem.isCompleted()) {
             binding.name.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-            binding.score.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+//            binding.score.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
             binding.dueTime.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+            binding.score.setBackgroundResource(R.drawable.green_circle_checked)
+            resizeBackgroundImage(binding.score.background)
+            binding.score.text = ""
         }
 
         binding.completeButton.setImageResource(taskItem.imageResource())
@@ -39,5 +52,29 @@ class TaskItemViewHolder(
             binding.dueTime.text = timeFormatter.format(taskItem.dueTime)
         } else
             binding.dueTime.text = ""
+    }
+
+    private fun resizeBackgroundImage(background: Drawable?) {
+        val currentBackgroundDrawable: Drawable? = background
+        if (currentBackgroundDrawable is BitmapDrawable) {
+            // Get the desired width and height in pixels
+            val desiredWidthPx = context.resources.getDimensionPixelSize(R.dimen.desired_width)
+            val desiredHeightPx = context.resources.getDimensionPixelSize(R.dimen.desired_height)
+
+            // Resize the drawable to the desired dimensions
+            val resizedDrawable = BitmapDrawable(
+                context.resources,
+                Bitmap.createScaledBitmap(
+                    currentBackgroundDrawable.bitmap,
+                    desiredWidthPx,
+                    desiredHeightPx,
+                    false
+                )
+            )
+
+            // Set the resized drawable as the new background
+            binding.score.background = resizedDrawable
+            binding.score.gravity = Gravity.CENTER
+        }
     }
 }
